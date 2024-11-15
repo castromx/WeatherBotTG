0d2804024569b26296e20220d472d10216d1b3e9
castromx <96194271+castromx@users.noreply.github.com>
2024-11-15 19:38:07 +0100

    upd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from aiogram.filters import CommandStart
 from dotenv import load_dotenv
 from aiogram.utils.i18n import I18n, gettext as _
@@ -23,7 +23,8 @@ user_language = {}
 async def welcome(message: Message):
     lang = user_language.get(message.from_user.id, 'en')
     i18n.ctx_locale.set(lang)
-    await message.reply(_("welcome"))
+    print(f"Language set to {lang} for user {message.from_user.id}")
+    await message.reply(_("Hello, type a /weather [city] to get information or /language to choose language!"))
 
 
 @dp.message(F.text.startswith('/weather'))
@@ -35,22 +36,28 @@ async def weather(message: Message):
         info = await get_async_weather(city, lang)
         await message.answer(info)
     except IndexError:
-        await message.answer(_("enter_city"))
+        await message.answer(_("Please provide a city name after /weather"))
+
 
 
 @dp.message(F.text.startswith('/language'))
 async def set_language(message: Message):
-    try:
-        lang = message.text.split(' ')[1]
-        if lang in ['en', 'uk']:
-            user_language[message.from_user.id] = lang
-            i18n.ctx_locale.set(lang)
-            await message.reply(_("language_set"))
-        else:
-            await message.reply(_("available_languages"))
-    except IndexError:
-        await message.reply(_("provide_language_code"))
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="English 🇺🇸", callback_data="lang_en"),
+            InlineKeyboardButton(text="Українська 🇺🇦", callback_data="lang_uk")
+        ]
+    ])
+    await message.reply(_("Choose your language:"), reply_markup=keyboard)
 
+
+
+@dp.callback_query(F.data.startswith("lang_"))
+async def handle_language_choice(callback: CallbackQuery):
+    lang = callback.data.split('_')[1]
+    user_language[callback.from_user.id] = lang
+    await callback.message.edit_text(f"Language set to {'English' if lang == 'en' else 'Українська'}")
+    await callback.answer()
 
 async def main() -> None:
     bot = Bot(TOKEN)
